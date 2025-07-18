76b6a43ac21484b30f456b3758630c33ef8ba051
Hazem Sayed <hazemsayed@Hazems-MacBook-Pro-2.local>
2025-07-17 20:14:50 -0500

    update synthetitc and scoring mechansim

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class LocalConfig(Config):
     PORT = int(os.getenv("PORT", 5001))
 
 class CloudRunConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql://root:hazemshokry@34.55.195.124:3306/traindb2")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql://root:hazemshokry@34.55.195.124:3306/db1")
     PORT = int(os.getenv("PORT", 8080))
 
 def get_config():
