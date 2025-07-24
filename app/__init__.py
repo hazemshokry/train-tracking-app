1d8ef45d3475a27c446cfc9a6f3942fec686086a
Hazem Sayed <hazemsayed@Hazems-MacBook-Pro-2.local>
2025-07-24 08:05:40 -0500

    allow cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,33 @@
 # app/__init__.py
 from flask import Flask, render_template_string
+from flask_cors import CORS
 from app.config import get_config
 from app.extensions import db
 from app.routes import api  # Import the Api instance with namespaces
 
 def create_app(config_class=None):
     app = Flask(__name__)
+
+    # Configure CORS to allow requests from any origin for specific routes
+    # and to handle the necessary headers and methods.
+    CORS(app, resources={
+        r"/stations/*": {
+            "origins": "*",
+            "methods": ["GET", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization", "Accept"]
+        },
+        r"/trains/*": {
+            "origins": "*",
+            "methods": ["GET", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization", "Accept"]
+        },
+        r"/reports/*": {
+            "origins": "*",
+            "methods": ["POST", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization", "Accept"]
+        }
+    })
+
     app.config.from_object(config_class or get_config())
 
     @app.route("/redoc")
